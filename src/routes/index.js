14bf5f2f363ffde425d6c2479592c308303ad11a
James March <54935498+Fokkos@users.noreply.github.com>
2024-03-07 17:43:58 +0000

    Plant form now creates JSON object

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,4 +20,40 @@ router.get('/form-test', (req, res) => {
   renderLayout(res, 'form_test', { title: 'Form Test' });
 });
 
+router.post('/form-test', (req, res) => {
+  plant = {
+    name: req.body.name,
+    description: req.body.description,
+    dateTimeSeen: new Date(req.body.dateTimeSeen),
+    size: parseFloat(req.body.size),
+    sunExposure: req.body.sunExposure,
+    colour: req.body.colour}
+
+  if (req.body.hasFlowers === 'true') {
+    plant.hasFlowers = true
+  }else{
+    plant.hasFlowers = false
+  }
+  if (req.body.hasLeaves === 'true') {
+    plant.hasLeaves = true
+  }else{
+    plant.hasLeaves = false
+  }
+
+  if (req.body.hasFruit === 'true') {
+    plant.hasFruit = true
+  }else{
+    plant.hasFruit = false
+  }
+
+  if (req.body.hasSeeds === 'true') {
+    plant.hasSeeds = true
+  }else{
+    plant.hasSeeds = false
+  }
+
+  console.log(plant);
+  renderLayout(res, 'form_test', { title: 'Form Test' });
+});
+
 module.exports = router;
