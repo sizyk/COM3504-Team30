260a27e8fa3839a2df067a1c74f0b787b42dfbf8
Jack Sanders <jacksanders404@gmail.com>
2024-03-13 15:56:18 +0000

    Actually fixed hover on mobile

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,15 +1,37 @@
+// Store all elements that require hover functionality on mobile
+const TOUCH_HOVER_ELEMS = document.getElementsByClassName('touchscreen-hover');
+
+/**
+ * @returns {boolean} whether or not current device is touchscreen
+ */
 function isTouchDevice() {
   return (('ontouchstart' in window)
     || (navigator.maxTouchPoints > 0)
     || (navigator.msMaxTouchPoints > 0));
 }
 
+// Code to allow hovering on touchscreen devices
 if (isTouchDevice()) {
-  const touchHoverElems = document.getElementsByClassName('touchscreen-hover');
+  // Unhover everything when user clicks anywhere that isn't hoverable
+  document.addEventListener('click', () => {
+    Array.from(TOUCH_HOVER_ELEMS).forEach((elem) => {
+      elem.classList.remove('hover');
+    });
+  });
 
-  Array.from(touchHoverElems).forEach((elem) => {
+  // Add click listener to all elements that require hovering on touchscreen
+  Array.from(TOUCH_HOVER_ELEMS).forEach((elem) => {
     elem.addEventListener('click', (e) => {
-      e.target.closest('.plant-card').classList.toggle('hover');
+      // Unhover all other elements that are not the current one, and not ancestors of current one
+      Array.from(TOUCH_HOVER_ELEMS).forEach((otherElem) => {
+        if (!otherElem.contains(elem)) {
+          otherElem.classList.remove('hover');
+        }
+      });
+
+      elem.classList.toggle('hover');
+
+      e.stopPropagation(); // Prevent document click listener from being called
     });
   });
 }
