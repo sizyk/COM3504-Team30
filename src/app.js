0f05c51e653aca7e466052a7e7a8dc9a104ff177
andreas <eandreas1@sheffield.ac.uk>
2024-03-14 18:55:31 +0000

    Feat(Single Plant View): Init

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes');
+const plantRouter = require('./routes/plant');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/plant', plantRouter);
 
 app.use('/public', express.static('public'));
 
