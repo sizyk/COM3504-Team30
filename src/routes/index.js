2456712572841896cb39113682710dbad0cc18b1
James March <54935498+Fokkos@users.noreply.github.com>
2024-03-16 14:23:28 +0000

    edit functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const renderLayout = require('../helpers/layout-renderer');
 
 const router = express.Router();
 const plants = require('../controllers/plants');
+const PlantModel = require('../models/plants');
 
 /* IMAGE CODE (move into seperate route eventually) */
 
@@ -62,6 +63,42 @@ router.post('/create-plant', upload.single('image'), (req, res) => {
   log(result);
 
   res.redirect('/');
+});
+
+router.post('/edit-plant', upload.single('image'), async (req, res) => {
+  const plantData = req.body;
+
+  const plant = { // Get data from the form
+    author: 'placeholder', // replace with user when implemented
+    name: plantData.name,
+    description: plantData.description,
+    dateTimeSeen: new Date(plantData.dateTimeSeen),
+    size: parseFloat(plantData.size),
+    sunExposure: plantData.sunExposure,
+    colour: plantData.colour,
+    longitude: plantData.longitude,
+    latitude: plantData.latitude,
+    hasFlowers: plantData.hasFlowers === 'true', // Checkboxes have no value if not checked, so manually check whether they are true
+    hasLeaves: plantData.hasLeaves === 'true',
+    hasFruit: plantData.hasFruit === 'true',
+    hasSeeds: plantData.hasSeeds === 'true',
+  };
+
+  if (req.file) { // If a new image is uploaded, update the image path
+    const filepath = req.file.path.replace(/\\/g, '/');
+    plant.image = filepath;
+  }
+
+  // TODO delete the old image if a new one is uploaded
+
+  // Update the plant in the database
+  try {
+    const updatedModel = await PlantModel.findByIdAndUpdate(plantData._id, plant, {new: true});
+    // Redirect to some page or send a response indicating success
+    res.redirect('/');
+  } catch (error) {
+    log(error)
+  }
 });
 
 module.exports = router;
