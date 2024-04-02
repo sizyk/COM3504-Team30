535f661811ab13cda93ed835d25140b8dbc4b3e4
Jack Sanders <jacksanders404@gmail.com>
2024-04-02 16:11:01 +0100

    Chat responsiveness

--- src/controllers/socket-io.js
+++ src/controllers/socket-io.js
@@ -1,5 +1,3 @@
-const ChatModel = require('../models/chat');
-
 exports.init = (io) => {
   io.sockets.on('connection', (socket) => {
     try {
@@ -7,22 +5,11 @@ exports.init = (io) => {
        * create or joins a room
        */
       socket.on('create or join', async (room) => {
-        // const messages = await ChatModel.find({ plant: room });
         socket.join(room);
-        // io.to(room).emit('oldMessages', messages);
-        // console.log(messages);
-      });
-
-      socket.on('oldMessages', async (room) => {
-        const messages = await ChatModel.find({ plant: room });
-        io.to(room).emit('oldMessages', messages);
       });
 
       socket.on('chat', (room, params) => {
         io.to(room).emit('chat', params);
-      });
-
-      socket.on('disconnect', () => {
       });
     } catch (e) { /* empty */ }
   });
