43d823c6efcd6d8625a123bcd53f850333c22828
Jack Sanders <jacksanders404@gmail.com>
2024-04-17 20:16:08 +0100

    Map tab works

---
+++ public/scripts/global-scripts/tabs.mjs
@@ -0,0 +1,40 @@
+function activateTab(buttonList, tabList, tabID) {
+  buttonList.forEach((button) => {
+    button.classList.remove('active');
+
+    if (button.dataset.target === tabID) {
+      button.classList.add('active');
+    }
+  });
+
+  tabList.forEach((tab) => {
+    tab.classList.remove('active');
+
+    if (tab.id === tabID) {
+      tab.classList.add('active');
+      if ('hasLeafletMap' in tab.dataset) {
+        // Force leaflet map to invalidate size when removing display: hidden
+        tab.querySelector('[data-is-leaflet]').dispatchEvent(new Event('leaflet-invalidate'));
+      }
+    }
+  });
+}
+
+/**
+ * Initialises functionality of all tabs on the page
+ */
+export default function initTabs() {
+  // Detect all tab groups on the page
+  document.querySelectorAll('.tabs').forEach((tabGroup) => {
+    const tabButtons = tabGroup.querySelectorAll(':scope >.tab-buttons>.tab-button');
+    const tabList = tabGroup.querySelectorAll(':scope>.tab-container>.tab-content');
+
+    tabButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        activateTab(tabButtons, tabList, button.dataset.target);
+      });
+    });
+
+    activateTab(tabButtons, tabList, tabButtons[0].dataset.target);
+  });
+}
--- public/scripts/global.js
+++ public/scripts/global.js
@@ -7,12 +7,15 @@ import initTouchScreen from './global-scripts/touchHover.mjs';
 import initSyncing from './global-scripts/syncing.mjs';
 import initSW from './global-scripts/serviceWorker.mjs';
 import initLogin from './global-scripts/login.mjs';
+import initTabs from './global-scripts/tabs.mjs';
 
 initSW();
 initThemeToggle();
 initModals();
 initTouchScreen();
 initLogin();
+initTabs();
+
 // Only enable syncing if not in offline mode
 if (!(Object.prototype.hasOwnProperty.call(window, 'plantsAppOffline') || window.plantsAppOffline === false)) {
   initSyncing();
